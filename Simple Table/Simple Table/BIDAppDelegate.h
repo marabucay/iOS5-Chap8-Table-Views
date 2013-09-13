776215548178f84a4fdbe4fadc9f9c27b48add80
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-13 17:25:39 +0800

    Created Simple table Project.

---
+++ Simple Table/Simple Table/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  Simple Table
+//
+//  Created by Maria Alice C. Lim on 9/13/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
---
+++ Simple Table/Simple Table/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Simple Table
+//
+//  Created by Maria Alice C. Lim on 9/13/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
