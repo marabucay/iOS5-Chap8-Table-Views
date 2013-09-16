afc8e97ae61944569ee56ac462a908a9eefc19a2
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-16 11:10:10 +0800

    Created Cells Project.

---
+++ Cells/Cells/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Cells
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
